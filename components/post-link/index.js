a679c2f7b4ad5ad56c870c400529870a0340fbf6
Ive <ive.tourinho@itmov.com.br>
2019-03-08 10:22:15 -0300

    Express custom server

--- components/post-link/index.js
+++ components/post-link/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const PostLink = (props) => (
   <li>
-    <Link  as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
       <a>{props.title}</a>
     </Link>
   </li>
